047fe89c107e8a18621e31dfe312d0e864610bea
satyam chauhan <satyamipl1456@gmail.com>
2026-05-17 08:33:57 +0530

    production settings

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-ecommerce-dev-key-change-in-production-12345'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
